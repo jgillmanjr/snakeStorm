1c2c80fbafc4a7212b8f8107bd9eb4b7ccd4a0f2
Jason Gillman Jr <jgillman@liquidweb.com>
2014-07-30 09:29:11 -0400

    Added parameter methods

--- snakeStorm.py
+++ snakeStorm.py
@@ -19,7 +19,30 @@ class snakeStorm:
 		self.version		= version
 
 		self.baseURI		= 'https://api.stormondemand.com'
-		self.fullURI		= self.baseURI + '/' + self.version + '/' + self.method + '.json'
+		self.apiPort		= 443
+		self.apiFormat		= 'json'
+		self.fullURI		= '%s:%s/%s/%s.%s' % (self.baseURI, str(self.apiPort), self.version, self.method, self.apiFormat)
 
-	def testing(self):
-		print self.fullURI
+	## Local Parameter Methods ##
+	def addParam(self, key, value):
+		""" Add a single parameter. If already set, it will be overwritten"""
+		self.parameters[key] = value
+
+	def addParams(self, paramDict):
+		""" Add multiple parameters. If a parameter is already set, it will be overwritten. """
+		if type(paramDict) is dict:
+			for (key,value) in paramDict.iteritems():
+				self.parameters[key] = value
+
+	def clearParams(self):
+		""" Remove all set parameters """
+		self.parameters = None
+
+	def listParams(self):
+		""" A holdover from me being used to having variable visibility """
+		return self.parameters
+
+	def removeParam(self, key):
+		""" Remove a specific parameter by key """
+		if self.parameters.has_key(key):
+			del self.parameters[key]
