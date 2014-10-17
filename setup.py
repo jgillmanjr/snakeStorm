9b3f096e6e3fec288a01cfbe9f0291c88b5f9e86
Jason Gillman Jr <jgillman@liquidweb.com>
2014-10-17 07:36:18 -0400

    Typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	packages = ['snakeStorm'],
 	version ='0.1',
 	description = "A python module to work with Liquid Web's Storm API",
-	author = 'Jason Gillman Jr.'
+	author = 'Jason Gillman Jr.',
 	author_email = 'jason@rrfaae.com',
 	url = 'https://github.com/jgillmanjr/snakeStorm',
 	download_url = 'https://github.com/jgillmanjr/snakeStorm/archive/v0.1.tar.gz',
