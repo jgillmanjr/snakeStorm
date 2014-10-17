42be9490c78d3f974c8943554cdb8105bd8a7504
Jason Gillman Jr <jgillman@liquidweb.com>
2014-10-17 07:35:08 -0400

    Pypi stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'snakeStorm',
+	packages = ['snakeStorm'],
+	version ='0.1',
+	description = "A python module to work with Liquid Web's Storm API",
+	author = 'Jason Gillman Jr.'
+	author_email = 'jason@rrfaae.com',
+	url = 'https://github.com/jgillmanjr/snakeStorm',
+	download_url = 'https://github.com/jgillmanjr/snakeStorm/archive/v0.1.tar.gz',
+	keywords = ['REST', 'Liquid Web', 'Storm', 'API'],
+	classifiers = [],
+)
